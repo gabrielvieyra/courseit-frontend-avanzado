96ef14f4d53545dcbe568b6e620a01ed263753ba
gabriel <vieyra.gabriel08@gmail.com>
2021-05-29 10:50:02 -0300

    estilos en react

---
+++ ejercicio-meli-rutas/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Test from "./components/Test/Test";
+
+function App() {
+    return (
+        <>
+            <Test />
+        </>
+    );
+}
+
+export default App;
---
+++ ejercicio-meli-rutas/src/components/Test/Test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Test.css";
+
+function Test() {
+    return (
+        <>
+            <h1 className="primerTest">soy un test</h1>
+            <h2 className="secondTest">soy otro test</h2>
+        </>
+    );
+}
+
+export default Test;
