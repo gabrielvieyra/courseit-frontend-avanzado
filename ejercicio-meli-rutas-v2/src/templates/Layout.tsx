a235e6960157c40c98ea305af6fc31e868483269
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2022-09-09 19:56:04 -0300

    se agrego el componente layout

---
+++ ejercicio-meli-rutas-v2/src/templates/Layout.tsx
@@ -0,0 +1,12 @@
+// Styles
+import "./_layout.scss";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return <div className="layout">{children}</div>;
+};
+
+export default Layout;
